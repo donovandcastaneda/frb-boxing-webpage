8f2da4bc83d473ef169cfea5a2b5e9ae1f3f1f72
Donovan Castaneda <donovandcastaneda@gmail.com>
2022-11-18 18:30:20 -0600

    added coach card

--- src/components/CoachesCard.tsx
+++ src/components/CoachesCard.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-
-
-export function Coach {
+export function CoachCards(name: string, imgUrl: any, title: string) {
   return (
-   
+    <div>
+      <img
+        src={imgUrl}
+        alt="Coach"
+        className="w-full h-36 md:h-48 object-cover cursor-pointer"
+      />
+      <div className="w-full p-4">
+        <h1 className="text-lg md:text-xl mb-2 md:mb-3 ">{name}</h1>
+        <h2>{title}</h2>
+      </div>
+    </div>
   );
-};
+}
---
+++ src/pages/Coaches.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import CoachData from "../data/CoachData";
+import { CoachCards } from "../components/CoachCard";
+
+
+export function Coaches() 
+{
+    return (
+        <div className = "flex flex-col md:flex-row items-center">
+            <div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+                {Coaches.map((boxer)=>(
+                    <CoachCards
+                    name = {coach.name}
+                    imgUrl = {coach.imgUrl}
+                    status = {coach.title}
+                    />
+                ))}
+            </div>
+
+        </div>
+    )
+}
