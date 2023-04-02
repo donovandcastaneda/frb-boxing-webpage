0cc7c74112ee4d666e0829308f4af2f1e0616cc6
Donovan Castaneda <donovandcastaneda@gmail.com>
2023-04-02 15:22:51 -0500

    added pictures to home page (responsive)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,40 @@
 import { HomeEventCards } from "../components/HomeEventCard";
 import EventData from "../data/EventData";
 
-export function Home() {
+export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <div className="">
+      <div className="w-full h-full">
+        <picture>
+          <source
+            media="(max-width: 639px)"
+            srcSet="/assets/home-bg-sm.png"
+          />
+          <source
+            media="(min-width: 640px) and (max-width: 1120px)"
+            srcSet="/assets/home-bg-md.png"
+          />
+          <source
+            media="(min-width: 1024px)"
+            srcSet="/assets/home-bg-lg.png"
+          />
+          <img
+            src="/assets/home-bg.png"
+            className="w-full h-[1000px] object-cover"
+            alt=""
+          />
+        </picture>
       </div>
-      <div className="absolute bottom-60 w-full">
-        <HomeEventCards
-          imgUrl={undefined}
-          title={""}
-          description={""}
-          location={""}
-          date={new Date()}
-        />
+      <div className="">
+        <section className="absolute bottom-0 w-full bg-base-100">
+          <HomeEventCards
+            imgUrl={undefined}
+            title={""}
+            description={""}
+            location={""}
+            date={new Date()}
+          />
+        </section>
       </div>
     </div>
   );
