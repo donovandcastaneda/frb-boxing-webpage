0ed71dd296224947841e70bc5c04e59f21e4f2ee
Donovan Castaneda <donovandcastanedajr@gmail.com>
2023-05-20 19:48:19 -0500

    set up db connection for coaches and events

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const db = mysql2.createConnection({
 app.use(express.json());
 app.use(cors());
 
-
+//boxers
 
 app.get("/boxers", (req, res) => {
   const q = "SELECT * FROM frb.boxers";
@@ -61,6 +61,98 @@ app.put("/boxers/:id", (req, res) => {
   });
 });
 
+//coaches
+
+
+app.get("/coaches", (req, res) => {
+  const q = "SELECT * FROM frb.coaches";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+
+
+app.post("/coaches", (req, res) => {
+  const q = "INSERT INTO coaches (`name`,`desc`,`image`) VALUES (?)";
+  const values = [req.body.name, req.body.desc, req.body.image];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("coach has been created");
+  });
+});
+
+
+
+app.delete("/coaches/:id", (req, res) => {
+  const coachId = req.params.id;
+  const q = "DELETE FROM coaches WHERE id = ?";
+
+  db.query(q, [coachId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Coach has been deleted.");
+  });
+});
+
+
+app.put("/coaches/:id", (req, res) => {
+  const coacheId = req.params.id;
+  const q = "UPDATE coaches SET `name`= ?, `desc`= ?, `image`= ? WHERE id = ?";
+  const values = [req.body.name, req.body.desc, req.body.image];
+
+  db.query(q, [...values, coacheId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Coach has been updated.");
+  });
+});
+ 
+//events
+
+app.get("/events", (req, res) => {
+  const q = "SELECT * FROM frb.events";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+
+
+app.post("/coaches", (req, res) => {
+  const q = "INSERT INTO events (`name`,`desc`,`image`,`date`) VALUES (?)";
+  const values = [req.body.name, req.body.desc, req.body.image, req.body.date];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Event has been created");
+  });
+});
+
+
+
+app.delete("/events/:id", (req, res) => {
+  const eventId = req.params.id;
+  const q = "DELETE FROM coaches WHERE id = ?";
+
+  db.query(q, [eventId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Coach has been deleted.");
+  });
+});
+
+
+app.put("/coaches/:id", (req, res) => {
+  const coachId = req.params.id;
+  const q = "UPDATE coaches SET `name`= ?, `desc`= ?, `image`= ?,`date`= ?  WHERE id = ?";
+  const values = [req.body.name, req.body.desc, req.body.date];
+
+  db.query(q, [...values, coachId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Coach has been updated.");
+  });
+});
 
 
 app.listen(8800, () => {
